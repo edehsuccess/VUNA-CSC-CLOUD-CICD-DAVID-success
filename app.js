3b2affad2e6638c661730b4070ea0b024ef8b6c7
cyberspacex <126796642+oluwatay123@users.noreply.github.com>
2024-05-27 16:10:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send(`THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED
+WITH AWS. THIS PAGE INDICATES SUCCESS.
+YOURNAME: DAVID OLUWATAYO DAMILOLA 
+MATRIC NUMBER: VUG\ CSC\ 21 \ 6100`));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
