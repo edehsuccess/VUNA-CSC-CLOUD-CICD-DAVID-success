fad7910077835b6f559880b0542e490f81086811
edehsuccess <successedeh6@gmail.com>
2024-05-30 15:11:25 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 8080;
-app.use(express.static('public'));
+const port = 8080;
+app.use(express.static("public"));
 
 //app.get('/', (req, res) => res.send(`THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED
 //WITH AWS. THIS PAGE INDICATES SUCCESS.
