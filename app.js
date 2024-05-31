4b4b09d0eea795947657978a8013e4069b36dcd9
edehsuccess <successedeh6@gmail.com>
2024-05-31 12:58:03 +0100

    changes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 8080;
-app.use(express.static("public"));
 
-//app.get('/', (req, res) => res.send(`THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED
-//WITH AWS. THIS PAGE INDICATES SUCCESS.
-//YOURNAME: EDEH SUCCESS CHIZUBE
-//MATRIC NUMBER: VUG\ CSC\ 21 \ 5545`));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
